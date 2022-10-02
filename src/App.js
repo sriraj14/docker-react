5c7d85a43f974dcd60228e257fc3095b12469ea6
EC2 Default User <ec2-user@ip-172-31-65-19.ec2.internal>
2022-10-02 14:21:32 +0000

    added app text thru feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          i was changed on feature branch
         </a>
       </header>
     </div>
